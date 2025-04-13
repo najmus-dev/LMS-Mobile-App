bf683684ebb974a22b81048d498238fe01da3980
Najmus Saqib <najmusesaqib123@gmail.com>
2025-04-13 19:38:42 +0500

    first commit

---
+++ client/utils/uri.tsx
@@ -0,0 +1,3 @@
+// export const SERVER_URI = "https://lms-mobile-app-alde.onrender.com/api/v1";
+// export const SERVER_URI = "https://web-lms-server.onrender.com/api/v1";
+export const SERVER_URI = "http://192.168.251.74:8000/api/v1";
