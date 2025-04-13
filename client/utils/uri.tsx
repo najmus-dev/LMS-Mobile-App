d4d147546a8557c503b838464aaa728055a6bd2a
Najmus Saqib <najmusesaqib123@gmail.com>
2025-04-13 21:15:51 +0500

    more updates

--- client/utils/uri.tsx
+++ client/utils/uri.tsx
@@ -1,3 +1,5 @@
 // export const SERVER_URI = "https://lms-mobile-app-alde.onrender.com/api/v1";
 // export const SERVER_URI = "https://web-lms-server.onrender.com/api/v1";
-export const SERVER_URI = "http://192.168.251.74:8000/api/v1";
+// export const SERVER_URI = "http://192.168.251.74:8000/api/v1";
+
+export const SERVER_URI = "https://lms-mobile-app-2.onrender.com/api/v1";
--- server/app.ts
+++ server/app.ts
@@ -19,18 +19,18 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // cors => cross origin resource sharing
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
 // app.use(
 //   cors({
-//     origin: "*", // allow all for testing
+//     origin: ["http://localhost:3000"],
 //     credentials: true,
 //   })
 // );
+app.use(
+  cors({
+    origin: "*", // allow all for testing
+    credentials: true,
+  })
+);
 
 // api requests limit
 const limiter = rateLimit({
